4c1b9f7dca751b21a5342f662172589bca12ee41
rsymons22 <rhinoninja01@gmail.com>
2024-07-25 22:19:00 -0400

    Cleaned up tracking code, doesn't crash during execution. Detection calculates yaw alongside distance.

--- apriltag_detection.py
+++ apriltag_detection.py
@@ -4,11 +4,11 @@ import apriltag
 import os
 import math
 
-# cv displays will show on the pi, w/o this command program crashes
-#os.system("export DISPLAY=:0.0")
+# Command to display grahics on pi: export DISPLAY=:0.0
 
 HEIGHT_PX = 480
 WIDTH_PX = 640
+TAG_WIDTH_MM = 152.4 # 6in
 
 class ApriltagDetection():
    def __init__(self):
@@ -22,11 +22,38 @@ class ApriltagDetection():
 
       self.horiz = None
       self.vert = None
+      self.stop = False
+      self.distance = None
 
+   def calculate_distance(self, corners, theta):
+      """
+      Calculates the distance from the apriltag to the camera using the formula:
+
+      D = (real_width * focal length x) / pixels_width
+
+      :param corners: size 4 tuple of size 2 coordinate tuples of the 4 corners
+      :param theta: yaw angle of the april tag
+      :return: distance in inches
+
+      """
+      m1 = ((corners[0][0] + corners[3][0]) / 2, (corners[0][1] + corners[3][1]) / 2)
+      m2 = ((corners[1][0] + corners[2][0]) / 2, (corners[1][1] + corners[2][1]) / 2)
+
+      width_px = math.sqrt((m2[0] - m1[0])**2 + (m2[1] - m1[1])**2)
+      # Rotated april tags will appear to have a smaller "width", this can be corrected
+      # by using the formula: real_width = apparent_width / cos(theta)
+      width_px_adjusted = math.fabs(width_px / math.cos(theta))
+
+      # Focal length is in mm, so must first calculate dist in mm then convert to inches
+      dist_mm = (TAG_WIDTH_MM * self.newmtx[0][0]) / width_px_adjusted
+      dist_in = dist_mm / 25.4
+      self.distance = dist_in
 
    def run_detection(self):
       cap = cv.VideoCapture(0)
       while True:
+         if self.stop: break
+
          status, img = cap.read()
 
          dst = cv.undistort(img, self.mtx, self.dist, None, self.newmtx)
@@ -49,7 +76,7 @@ class ApriltagDetection():
             ptA = (int(ptA[0]), int(ptA[1]))
 
             center = (int(r.center[0]), int(r.center[1]))
-
+            
             # Calculate offsets
             self.horiz = center[0] - (WIDTH_PX / 2)
             self.vert = center[1] - (HEIGHT_PX / 2)
@@ -62,8 +89,8 @@ class ApriltagDetection():
             cv.line(dst, ptD, ptA, (0, 255, 0), 2)
 
             cv.circle(dst, center, 1, (0, 0, 255), 2)
-
-            #print(math.atan2(r.homography[1, 0], r.homography[0, 0]))
+            self.calculate_distance(r.corners, math.atan2(r.homography[1, 0], r.homography[0, 0]))
+            
          else:
             self.horiz = None
             self.vert = None
--- apriltag_tracking.py
+++ apriltag_tracking.py
@@ -1,51 +1,91 @@
+from time import sleep
 from apriltag_detection import ApriltagDetection
 from servo_motor import ServoMotor
 from beam_sensor import BeamSensor
 import threading
 
 THRESHOLD_PX = 10
+DEBUG = True
 
 atdetection = ApriltagDetection()
 motor = ServoMotor()
 beam = BeamSensor()
 
-at_thread = threading.Thread(target=atdetection.run_detection, daemon=True)
+stop = False
 
-turning_CW = False
-turning_CCW = False
+def track():
+    
+    at_thread = threading.Thread(target=atdetection.run_detection, daemon=True)
 
-at_thread.start()
+    turning_CW = False
+    turning_CCW = False
 
-try:
+    at_thread.start()
+
     while True:
+        if stop: break
 
-        if atdetection.horiz == None and not turning_CW and not turning_CCW: continue
+        # Take a snapshot of the horiz offset value, as atdetection.horiz can change to None even after the check
+        h = atdetection.horiz
 
-        if atdetection.horiz < -THRESHOLD_PX and not turning_CCW:
-            print("turning CCW")
+        # No tags detected
+        if h == None:
+            # Currently moving -> stop
+            if turning_CCW or turning_CW:
+                if DEBUG: print("Stopping [Lost target]")
+                motor.stop()
+
+                turning_CW = False
+                turning_CCW = False
+            # Not moving, just nothing to see -> continue
+            else:
+                continue
+
+        # Tag center within threshold -> stop
+        elif (turning_CCW or turning_CW) and (h >= -THRESHOLD_PX and h <= THRESHOLD_PX):
+            if DEBUG: print("Stopping [Centered]")
             motor.stop()
+
+            turning_CW = False
+            turning_CCW = False
+
+        # Tag is to the left and camera is not currently turning -> turn CCW
+        elif h < -THRESHOLD_PX and not turning_CCW:
+            # If turning other direction, stop the motor and wait a bit
+            if turning_CW:
+                if DEBUG: print("Stopping [Sudden direction change]")
+                motor.stop()
+                sleep(0.01)
+            
+            if DEBUG: print("Turning CCW")
             threading.Thread(target=motor.turn, args=(False,), daemon=True).start()
 
             turning_CCW = True
             turning_CW = False
-        elif atdetection.horiz > THRESHOLD_PX and not turning_CW:
-            print("turning CW")
-            motor.stop()
+
+        # Tag is to the right and camera is not currently turning -> turn CW
+        elif h > THRESHOLD_PX and not turning_CW:
+            # If turning other direction, stop the motor and wait a bit
+            if turning_CCW:
+                if DEBUG: print("Stopping [Sudden direction change]")
+                motor.stop()
+                sleep(0.01)
+            
+            if DEBUG: print("Turning CW")
             threading.Thread(target=motor.turn, args=(True,), daemon=True).start()
 
             turning_CW = True
             turning_CCW = False
 
-        elif (turning_CCW or turning_CW) and ((atdetection.horiz >= -THRESHOLD_PX and atdetection.horiz <= THRESHOLD_PX) or atdetection.horiz == None):
-            print("stopping")
-            motor.stop()
+            
+    atdetection.stop = True
 
-            turning_CW = False
-            turning_CCW = False
+if __name__ == "__main__":
+    threading.Thread(target=track, daemon=True).start()
 
-except KeyboardInterrupt:
+    input("Press enter to end tracking\n")
+
+    stop = True
+
     print("Done")
-finally:
-    print("cleaning up")
     motor.cleanup()
-
